a5829adf61e24fadbd7c99be5446e651a344fb72
Beatriz Ceschini <biaceschini@gmail.com>
2023-07-04 16:15:27 -0300

    Routes e hooks

--- aluroni/src/pages/Cardapio/Ordenador/index.tsx
+++ aluroni/src/pages/Cardapio/Ordenador/index.tsx
@@ -1,7 +1,7 @@
-import styles from "./Ordenador.module.scss";
-import opcoes from "./opcoes.json";
-import React, { useState } from "react";
-import classNames from "classnames";
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
 interface Props { 
@@ -19,25 +19,23 @@ export default function Ordenador({
     <button
       className={classNames({
         [styles.ordenador]: true,
-        [styles["ordenador--ativo"]]: ordenador !== ""
+        [styles['ordenador--ativo']]: ordenador !== ''
       })}
       onClick={() => setAberto(!aberto)}
       onBlur={() => setAberto(false)}
     >
-      <span>{nomeOrdenador || "Ordenar Por"}</span>
+      <span>{nomeOrdenador || 'Ordenar Por'}</span>
       {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} /> }
       <div className={classNames({
         [styles.ordenador__options]: true,
         [styles['ordenador__options--ativo']]: aberto
       })}>
         {opcoes.map(opcao => (
-          <div className={styles.ordenador__option} 
-          key={opcao.value} 
-          onClick={() => setOrdenador(opcao.value)}>
+          <div className={styles.ordenador__option} key={opcao.value} onClick={() => setOrdenador(opcao.value)}>
             {opcao.nome}
           </div>
         ))}
       </div>
     </button>
-  )
+  );
 }
