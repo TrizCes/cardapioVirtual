bda9b0f64f4c3601d609563caa364e4d9c064fa5
Beatriz Ceschini <biaceschini@gmail.com>
2023-07-06 23:53:19 -0300

    Finalização

--- aluroni/src/routes.tsx
+++ aluroni/src/routes.tsx
@@ -4,23 +4,23 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
-import Sobre from 'pages/Sobre';
 import Prato from 'pages/Prato';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <main className="container">
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
-          <Route path="/" element={<PaginaPadrao />}>
+          <Route path='/' element={<PaginaPadrao />} >
             <Route index element={<Inicio />} />
-            <Route path="cardapio" element={<Cardapio />} />
-            <Route path="sobre" element={<Sobre />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
-          <Route path="prato/:id" element={<Prato />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path='prato/:id' element={<Prato />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
